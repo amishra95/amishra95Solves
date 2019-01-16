c2c2102deb47cf442cb725e0d3ae964591dc12ba
Avanish Mishra <avanish.neham@gmail.com>
2019-01-16 22:43:25 +0530

    first attempt at this question ( First Submission) Incorrect and not done

---
+++ numbersWithSameConsequitiveDifferences.java
@@ -0,0 +1,21 @@
+package hi;
+
+public class numbersWithSameConsequitiveDifferences {
+
+	public int[] numsSameConsecDiff(int N, int K) {
+
+	    List<Integer> list = new ArrayList<Integer>();
+	    if(K == 0){
+	        return 
+	    }
+	    
+	}
+	        
+	    public int helper(int N, int K){
+	        List<Integer> list = new ArrayList<Integer>();
+	         int[] array = new int[9];
+	    for(int i = 0; i < array.length-1; i++){
+	            if(array[i])
+	            }
+	    }
+}

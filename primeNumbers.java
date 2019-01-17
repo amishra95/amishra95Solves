aa18a2feeea8703c63b5e766b7a42897927ba63a
Avanish Mishra <avanish.neham@gmail.com>
2019-01-17 15:18:08 +0530

    HackerEarth Question

---
+++ primeNumbers.java
@@ -0,0 +1,50 @@
+/* IMPORTANT: Multiple classes and nested static classes are supported */
+
+/*
+ * uncomment this if you want to read input.
+//imports for BufferedReader
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+//import for Scanner and other utility classes
+import java.util.*;
+*/
+
+// Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+        /* Sample code to perform I/O:
+         * Use either of these methods for input
+
+        //BufferedReader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String name = br.readLine();                // Reading input from STDIN
+        System.out.println("Hi, " + name + ".");    // Writing output to STDOUT
+
+        //Scanner
+        Scanner s = new Scanner(System.in);
+        String name = s.nextLine();                 // Reading input from STDIN
+        System.out.println("Hi, " + name + ".");    // Writing output to STDOUT
+
+        */
+
+        // Write your code here
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
+        
+    for(int i = 2; i <= N; i++){
+        boolean flag = false;
+        for(int j = 2; j <= Math.sqrt(i); j++){
+            if(i %j == 0){
+                flag = true;
+                break;
+            }
+        }
+        if(flag == false){
+            System.out.print(i + " ");
+        }
+    }
+
+    }
+}

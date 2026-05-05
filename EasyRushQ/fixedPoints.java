ccec9648d5e1ea95e5cd83e9cc30ed2c03775fc3
Avanish Mishra <avanish.neham@gmail.com>
2026-05-05 10:39:43 +0530

    Update fixedPoints.java

--- EasyRushQ/fixedPoints.java
+++ EasyRushQ/fixedPoints.java
@@ -1,3 +1,27 @@
+//Faster search
+class Solution {
+    public int fixedPoint(int[] arr) {
+       int low = 0;
+       int high = arr.length-1;
+       int count = -1;
+
+        while(low <= high){
+            int mid = low+ (high-low)/2;
+            if(arr[mid] == mid){
+                count = mid;
+                high = mid-1;
+            }  
+            else if(arr[mid] < mid){
+                low = mid+1;
+            }  
+            else{
+                high = mid-1;
+            }
+        }
+       return count;
+    }
+}
+
 // Brute force
 class Solution {
     public int fixedPoint(int[] arr) {

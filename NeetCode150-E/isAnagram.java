e7ea06e308c18eb2374b4e5c34759e101c164dff
Avanish Mishra <avanish.neham@gmail.com>
2026-08-10 10:33:40 +0530

    Create isAnagram.java

---
+++ NeetCode150-E/isAnagram.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(t.length() != s.length()){
+            return false;
+        }
+    
+    HashMap<Character, Integer> map1 = new HashMap<>();
+    HashMap<Character, Integer> map2 = new HashMap<>();
+
+    for(char b: s.toCharArray()){
+        map1.put(b, map1.getOrDefault(b,0)+1);
+    }
+
+    for(char c: t.toCharArray()){
+        map2.put(c, map2.getOrDefault(c,0)+1);
+    }
+
+    return map1.equals(map2);    
+    
+    }
+}
+// Solution 2 using Sort
+
+class Solution {
+    static public boolean isAnagram(String s, String t) {
+        char[] inputArray = s.toCharArray();
+        char[] expectedArray = t.toCharArray();
+        Arrays.sort(inputArray);
+        Arrays.sort(expectedArray);
+
+        return Arrays.equals(inputArray, expectedArray);
+    }
+}

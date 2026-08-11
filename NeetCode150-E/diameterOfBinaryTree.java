a15b5ae4187ec8a9f80ec43581d9cc4a0abc7d9a
Avanish Mishra <avanish.neham@gmail.com>
2026-08-11 11:29:30 +0530

    Update diameterOfBinaryTree.java

--- NeetCode150-E/diameterOfBinaryTree.java
+++ NeetCode150-E/diameterOfBinaryTree.java
@@ -14,6 +14,50 @@
  * }
  */
 class Solution {
+    private int sol = 0;
+
+    public int diameterOfBinaryTree(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+       maxDepth(root);
+        return sol;   
+        
+    }
+
+    public int maxDepth(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+
+    int left = maxDepth(root.left);
+    int right = maxDepth(root.right);
+    sol = Math.max(sol, left+right);
+
+    return Math.max(left, right)+1;
+
+    }
+
+    
+
+    
+}
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
     public int diameterOfBinaryTree(TreeNode root) {
         if(root == null){
             return 0;

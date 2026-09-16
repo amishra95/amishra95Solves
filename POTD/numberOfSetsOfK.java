77b9f22c7ec054e2fdb90d51ffe1bf1181f123bf
Avanish Mishra <avanish.neham@gmail.com>
2026-09-16 23:25:51 +0530

    Create numberOfSetsOfK.java

---
+++ POTD/numberOfSetsOfK.java
@@ -0,0 +1,24 @@
+// Sept 16, 2026 Incomplete solution
+class Solution {
+  public final int MOD = 1_000_000_007;
+
+    public int numberOfSets(int n, int k) {
+        if(n == 0){
+            return 0;
+        }
+       long[][] dp = new long[n+1][2];
+        dp[0][0] = 1;
+
+    for(int i = 2; i <= n; i++){
+        long[][] next = new long[n+1][2];
+        
+        
+
+    }
+
+    
+   
+    
+    
+    }
+}
